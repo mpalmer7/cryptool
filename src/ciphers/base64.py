d0eb8235f1875c3641b52384622c297eec919a6f
mpalmer7 <mitchpalmer12@gmail.com>
2019-10-21 12:49:51 -0400

    updated error handling for base64.py

--- src/ciphers/base64.py
+++ src/ciphers/base64.py
@@ -3,7 +3,7 @@ import binascii
 
 
 def decrypt(ciphertext, b=None):
-	'''Takes in base64 encoded data, either with b'xxxxx' or just xxxxx, and returns the decoded string.'''
+	"""Takes in base64 encoded data, either with b'xxxxx' or just xxxxx, and returns the decoded string."""
 	
 	if ciphertext.startswith("b'") and ciphertext.endswith("'"):
 		ciphertext = ciphertext[2:-1]  # fixes formatting; data input is always of type(str)
@@ -11,10 +11,12 @@ def decrypt(ciphertext, b=None):
 		yield b64decode(ciphertext).decode()
 	except binascii.Error:
 		pass
+	except UnicodeDecodeError:
+		pass
 
 
 def encrypt(plaintext, key=None):
-	'''Takes in data, casts it as byte, and returns the base64 encoded string.''' 
+	"""Takes in data, casts it as byte, and returns the base64 encoded string."""
 
 	try:
 		return b64encode(plaintext.encode()).decode()
