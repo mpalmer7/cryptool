90ef074f08423adb0fa1d9bc41821e9e1f033584
mpalmer7 <mitchpalmer12@gmail.com>
2019-10-22 17:31:35 -0400

    Refactored atbash.py & added docstrings

--- src/ciphers/atbash.py
+++ src/ciphers/atbash.py
@@ -11,25 +11,25 @@ ldict = {'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v', 'f': 'u', 'g': 't',
          'v': 'e', 'w': 'd', 'x': 'c', 'y': 'b', 'z': 'a'}
 
 
-def bothcrypt(inp):
+def atb(inp_str):
+    """Given a string as input, calculate the atbash cipher on it and return the result as a string."""
     opt = ''
-    for letter in list(inp):
+    for letter in list(inp_str):
         if letter in udict.keys():
             opt += str(udict.get(letter))
         elif letter in ldict.keys():
             opt += str(ldict.get(letter))
         else:
             opt += letter
-    return opt
+    if opt != '':
+        return opt
 
 
 def decrypt(ciphertext, nullthing=None):
-    # decryption
-    plaintext = bothcrypt(ciphertext)
-    # output
-    if plaintext != '':
-        yield plaintext
+    """Decrypts the atbash cipher, yields a string as the result."""
+    yield atb(ciphertext)
 
 
 def encrypt(inp, key=None):
-    return bothcrypt(inp)
+    """Encrypts the atbash cipher, returns a string as the result."""
+    return atb(inp)
